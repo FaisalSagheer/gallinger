48de27246442c658707a0956f9b78cdf2ed0f201
faisalsagheer <faisalsagheerqureshi@gmail.com>
2025-10-07 19:49:46 +0500

    chgs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    // Additional common configurations:
-    server: {
-        port: 3000,
-        open: true, // Automatically open browser
-    },
-    build: {
-        outDir: 'dist',
-        sourcemap: true,
-    },
-    resolve: {
-        alias: {
-            '@': '/src', // Path alias for imports
-        },
-    },
+  plugins: [react()],
+  base:"/"
 })
